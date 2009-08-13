5d30f603865855b4447a171140e30eb044849d76
Carl Masak <cmasak@gmail.com>
2009-08-13 16:04:17 +0200

    [Templater] removed unused import

--- plugins/net.bioclipse.sdk/src/net/bioclipse/sdk/templating/Templater.java
+++ plugins/net.bioclipse.sdk/src/net/bioclipse/sdk/templating/Templater.java
@@ -14,7 +14,6 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.io.Reader;
-import java.io.StringWriter;
 import java.io.UnsupportedEncodingException;
 import java.util.ArrayList;
 import java.util.List;
