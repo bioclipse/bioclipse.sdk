6b4ff407c0f039c92121eb5d8a7ec03c1311aba2
Egon Willighagen <egon.willighagen@gmail.com>
2009-08-13 11:33:49 +0200

    Copied in code from the 'New QSAR Project' wizard, and it now creates a new project! yahoo!

--- plugins/net.bioclipse.sdk/src/net/bioclipse/sdk/wizards/ManagerPluginNewWizard.java
+++ plugins/net.bioclipse.sdk/src/net/bioclipse/sdk/wizards/ManagerPluginNewWizard.java
@@ -1,4 +1,5 @@
-/* Copyright (c) 2009  Egon Willighagen <egonw@users.sf.net>
+/* Copyright (c) 2008-2009  Ola Spjuth <olas@users.sf.net>
+ *                    2009  Egon Willighagen <egonw@users.sf.net>
  *
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
@@ -9,151 +10,155 @@
  ******************************************************************************/
 package net.bioclipse.sdk.wizards;
 
+import java.io.ByteArrayInputStream;
+import java.io.IOException;
+import java.lang.reflect.InvocationTargetException;
+
+import org.eclipse.core.resources.ICommand;
+import org.eclipse.core.resources.IContainer;
+import org.eclipse.core.resources.IFile;
+import org.eclipse.core.resources.IFolder;
+import org.eclipse.core.resources.IProject;
+import org.eclipse.core.resources.IProjectDescription;
+import org.eclipse.core.resources.IWorkspaceRoot;
+import org.eclipse.core.resources.ResourcesPlugin;
+import org.eclipse.core.runtime.CoreException;
+import org.eclipse.core.runtime.IPath;
+import org.eclipse.core.runtime.IProgressMonitor;
+import org.eclipse.core.runtime.NullProgressMonitor;
+import org.eclipse.core.runtime.Platform;
+import org.eclipse.core.runtime.SubProgressMonitor;
 import org.eclipse.jface.viewers.IStructuredSelection;
 import org.eclipse.jface.wizard.Wizard;
 import org.eclipse.ui.INewWizard;
 import org.eclipse.ui.IWorkbench;
-import org.eclipse.core.runtime.*;
-import org.eclipse.jface.operation.*;
-import java.lang.reflect.InvocationTargetException;
-import org.eclipse.jface.dialogs.MessageDialog;
-import org.eclipse.jface.viewers.ISelection;
-import org.eclipse.core.resources.*;
-import org.eclipse.core.runtime.CoreException;
-import java.io.*;
-import org.eclipse.ui.*;
-import org.eclipse.ui.ide.IDE;
+import org.eclipse.ui.actions.WorkspaceModifyOperation;
+import org.eclipse.ui.dialogs.WizardNewProjectCreationPage;
 
 /**
- * This is a sample new wizard. Its role is to create a new file 
- * resource in the provided container. If the container resource
- * (a folder or a project) is selected in the workspace 
- * when the wizard is opened, it will accept it as the target
- * container. The wizard creates one file with the extension
- * "mpe". If a sample multi-page editor (also available
- * as a template) is registered for the same extension, it will
- * be able to open it.
+ * New Project Wizard that sets up a Bioclipse plugin with a Bioclipse manager.
  */
-
 public class ManagerPluginNewWizard extends Wizard implements INewWizard {
-	private ManagerPluginNewWizardPage page;
-	private ISelection selection;
 
-	/**
-	 * Constructor for ManagerPluginNewWizard.
-	 */
-	public ManagerPluginNewWizard() {
-		super();
-		setNeedsProgressMonitor(true);
-	}
-	
-	/**
-	 * Adding the page to the wizard.
-	 */
+    private WizardNewProjectCreationPage fFirstPage;
 
-	public void addPages() {
-		page = new ManagerPluginNewWizardPage(selection);
-		addPage(page);
-	}
+    private IWorkbench workbench;
+    private IStructuredSelection selection;
+    
+    private final static String TITLE = "New Bioclipe Manager Plugin Project";
 
-	/**
-	 * This method is called when 'Finish' button is pressed in
-	 * the wizard. We will create an operation and run it
-	 * using wizard as execution context.
-	 */
-	public boolean performFinish() {
-		final String containerName = page.getContainerName();
-		final String fileName = page.getFileName();
-		IRunnableWithProgress op = new IRunnableWithProgress() {
-			public void run(IProgressMonitor monitor) throws InvocationTargetException {
-				try {
-					doFinish(containerName, fileName, monitor);
-				} catch (CoreException e) {
-					throw new InvocationTargetException(e);
-				} finally {
-					monitor.done();
-				}
-			}
-		};
-		try {
-			getContainer().run(true, false, op);
-		} catch (InterruptedException e) {
-			return false;
-		} catch (InvocationTargetException e) {
-			Throwable realException = e.getTargetException();
-			MessageDialog.openError(getShell(), "Error", realException.getMessage());
-			return false;
-		}
-		return true;
-	}
-	
-	/**
-	 * The worker method. It will find the container, create the
-	 * file if missing or just replace its contents, and open
-	 * the editor on the newly created file.
-	 */
+    public ManagerPluginNewWizard() {
+        super();
+        setWindowTitle(TITLE);
+    }
 
-	private void doFinish(
-		String containerName,
-		String fileName,
-		IProgressMonitor monitor)
-		throws CoreException {
-		// create a sample file
-		monitor.beginTask("Creating " + fileName, 2);
-		IWorkspaceRoot root = ResourcesPlugin.getWorkspace().getRoot();
-		IResource resource = root.findMember(new Path(containerName));
-		if (!resource.exists() || !(resource instanceof IContainer)) {
-			throwCoreException("Container \"" + containerName + "\" does not exist.");
-		}
-		IContainer container = (IContainer) resource;
-		final IFile file = container.getFile(new Path(fileName));
-		try {
-			InputStream stream = openContentStream();
-			if (file.exists()) {
-				file.setContents(stream, true, true, monitor);
-			} else {
-				file.create(stream, true, monitor);
-			}
-			stream.close();
-		} catch (IOException e) {
-		}
-		monitor.worked(1);
-		monitor.setTaskName("Opening file for editing...");
-		getShell().getDisplay().asyncExec(new Runnable() {
-			public void run() {
-				IWorkbenchPage page =
-					PlatformUI.getWorkbench().getActiveWorkbenchWindow().getActivePage();
-				try {
-					IDE.openEditor(page, file, true);
-				} catch (PartInitException e) {
-				}
-			}
-		});
-		monitor.worked(1);
-	}
-	
-	/**
-	 * We will initialize file contents with a sample text.
-	 */
+    public void addPages() {
+        super.addPages();
+        fFirstPage = new WizardNewProjectCreationPage(TITLE);
+        fFirstPage.setTitle(TITLE);
+        fFirstPage.setDescription("Create a " + TITLE);
+        addPage(fFirstPage);
+    }
 
-	private InputStream openContentStream() {
-		String contents =
-			"This is the initial file contents for *.mpe file that should be word-sorted in the Preview page of the multi-page editor";
-		return new ByteArrayInputStream(contents.getBytes());
-	}
+    @Override
+    public boolean performFinish() {
+        try {
+            WorkspaceModifyOperation op =
+                new WorkspaceModifyOperation() {
 
-	private void throwCoreException(String message) throws CoreException {
-		IStatus status =
-			new Status(IStatus.ERROR, "net.bioclipse.sdk", IStatus.OK, message, null);
-		throw new CoreException(status);
-	}
+                @Override
+                protected void execute(IProgressMonitor monitor)
+                throws CoreException, InvocationTargetException,
+                InterruptedException {
+                    createProject(monitor != null ?
+                            monitor : new NullProgressMonitor());
 
-	/**
-	 * We will accept the selection in the workbench to see if
-	 * we can initialize from it.
-	 * @see IWorkbenchWizard#init(IWorkbench, IStructuredSelection)
-	 */
-	public void init(IWorkbench workbench, IStructuredSelection selection) {
-		this.selection = selection;
-	}
+                }
+            };
+            getContainer().run(false,true,op);
+        } catch(InvocationTargetException x) {
+            x.printStackTrace();
+            return false;
+        } catch(InterruptedException x) {
+            return false;
+        }
+        return true;
+    }
+
+    public void init(IWorkbench workbench, IStructuredSelection selection) {
+        this.workbench = workbench;
+        this.selection = selection;
+        setWindowTitle(TITLE);
+    }
+
+    protected void createProject(IProgressMonitor monitor) {
+        monitor.beginTask("Creating the " + TITLE, 50);
+        try {
+            //Get WS root
+            IWorkspaceRoot root = ResourcesPlugin.getWorkspace().getRoot();
+            monitor.subTask("Creating directories");
+
+            //Create the project
+            IProject project = root.getProject(fFirstPage.getProjectName());
+
+            //Add natures and builders
+            IProjectDescription description = ResourcesPlugin.getWorkspace().
+                newProjectDescription(project.getName());
+            if(!Platform.getLocation().equals(fFirstPage.getLocationPath()))
+                description.setLocation(fFirstPage.getLocationPath());
+            description.setNatureIds(new String[] {
+                "org.eclipse.pde.PluginNature",
+                "org.eclipse.jdt.core.javanature"
+            });
+            ICommand javaBuilder = description.newCommand();
+            javaBuilder.setBuilderName("org.eclipse.jdt.core.javabuilder");
+            ICommand mfBuilder = description.newCommand();
+            mfBuilder.setBuilderName("org.eclipse.pde.ManifestBuilder");
+            description.setBuildSpec(new ICommand[] {
+                javaBuilder,
+                mfBuilder
+            });
+            project.create(description, monitor);
+            monitor.worked(10);
+
+            //Open project
+            project.open(monitor);
+
+            //Create folders
+            IPath projectPath = project.getFullPath(),
+            molPath = projectPath.append("src");
+            IFolder molFolder = root.getFolder(molPath);
+            createFolderHelper(molFolder, monitor);
+            monitor.worked(10);
+
+            //Create files (qsar.xml)
+            monitor.subTask("Creating files");
+            IPath qsarPath = projectPath.append("plugin.xml");
+            IFile qsarFile = root.getFile(qsarPath);
+            ByteArrayInputStream bos = new ByteArrayInputStream("".getBytes());
+            qsarFile.create(bos, true, new SubProgressMonitor(monitor,10));
+            bos.close();
+        } catch(CoreException x) {
+            x.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+            monitor.done();
+        }
+    }
+
+    private void createFolderHelper (IFolder folder, IProgressMonitor monitor) {
+        try {
+            if(!folder.exists()) {
+                IContainer parent = folder.getParent();
+
+                if (parent instanceof IFolder && (!((IFolder)parent).exists()))
+                    createFolderHelper((IFolder)parent, monitor);
+
+                folder.create(false,true,monitor);
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
 }
