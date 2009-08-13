8c951c5d9327fba14d586fb3582427a8942efe33
Egon Willighagen <egon.willighagen@gmail.com>
2009-08-13 11:40:25 +0200

    Cleaned up code a bit.
    * removed nonsense JavaDoc
    * added missing periods at the end of first JavaDoc sentences
    * renamed a variable to describe what it 'is'

--- plugins/net.bioclipse.sdk/src/net/bioclipse/sdk/Activator.java
+++ plugins/net.bioclipse.sdk/src/net/bioclipse/sdk/Activator.java
@@ -14,54 +14,39 @@ import org.eclipse.ui.plugin.AbstractUIPlugin;
 import org.osgi.framework.BundleContext;
 
 /**
- * The activator class controls the plug-in life cycle
+ * The activator class controls the plug-in life cycle.
  */
 public class Activator extends AbstractUIPlugin {
 
-	// The plug-in ID
 	public static final String PLUGIN_ID = "net.bioclipse.sdk";
-
-	// The shared instance
-	private static Activator plugin;
+	private static Activator sharedInstance;
 	
-	/**
-	 * The constructor
-	 */
-	public Activator() {
-	}
+	public Activator() {}
 
-	/*
-	 * (non-Javadoc)
-	 * @see org.eclipse.ui.plugin.AbstractUIPlugin#start(org.osgi.framework.BundleContext)
-	 */
 	public void start(BundleContext context) throws Exception {
 		super.start(context);
-		plugin = this;
+		sharedInstance = this;
 	}
 
-	/*
-	 * (non-Javadoc)
-	 * @see org.eclipse.ui.plugin.AbstractUIPlugin#stop(org.osgi.framework.BundleContext)
-	 */
 	public void stop(BundleContext context) throws Exception {
-		plugin = null;
+		sharedInstance = null;
 		super.stop(context);
 	}
 
 	/**
-	 * Returns the shared instance
+	 * Returns the shared instance.
 	 *
 	 * @return the shared instance
 	 */
 	public static Activator getDefault() {
-		return plugin;
+		return sharedInstance;
 	}
 
 	/**
 	 * Returns an image descriptor for the image file at the given
-	 * plug-in relative path
+	 * plug-in relative path.
 	 *
-	 * @param path the path
+	 * @param path the path to the image
 	 * @return the image descriptor
 	 */
 	public static ImageDescriptor getImageDescriptor(String path) {
