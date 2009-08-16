e0c0932103adf438a5ccae183e7d483da132d334
Egon Willighagen <egon.willighagen@gmail.com>
2009-08-16 15:03:25 +0200

    Added initial templates of a unit test plugin

---
+++ plugins/net.bioclipse.sdk/templates/net.bioclipse.foo.tests/src/JavaManagerPluginTest.java
@@ -0,0 +1,22 @@
+/*******************************************************************************
+ * Copyright (c) 2009  Egon Willighagen <egonw@users.sf.net>
+ * 
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * www.eclipse.org—epl-v10.html <http://www.eclipse.org/legal/epl-v10.html>
+ * 
+ * Contact: http://www.bioclipse.net/    
+ ******************************************************************************/
+package ${managerName}.tests;
+
+import org.junit.BeforeClass;
+
+public class Java${managerName}PluginTest
+    extends Abstract${managerName}PluginTest {
+
+    @BeforeClass public static void setup() {
+        rdf = ${managerName}.Activator.getDefault().getJavaManager();
+    }
+
+}
