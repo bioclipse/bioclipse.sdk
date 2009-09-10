871f1becbd0d9ca0cc80bad913b09adfdb4607d9
Egon Willighagen <egon.willighagen@gmail.com>
2009-09-10 16:12:14 +0200

    Added missing copyright/license statements

--- plugins/net.bioclipse.sdk/src/net/bioclipse/sdk/pdewizard/ManagerTestWizard.java
+++ plugins/net.bioclipse.sdk/src/net/bioclipse/sdk/pdewizard/ManagerTestWizard.java
@@ -1,3 +1,12 @@
+/* Copyright (c) 2009  Arvid Berg <arvid.berg@farmbio.uu.se>
+ *
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contact: http://www.bioclipse.net/
+ ******************************************************************************/
 package net.bioclipse.sdk.pdewizard;
 
 import org.eclipse.pde.ui.IFieldData;
